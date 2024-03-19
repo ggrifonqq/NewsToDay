ccdd9210c5c8e94b24002da00072baad89a63c30
ggrifonqq <112392189+ggrifonqq@users.noreply.github.com>
2024-03-19 17:31:10 +0300

    init
    init

---
+++ NewsToDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsToDay
+//
+//  Created by baikal on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
